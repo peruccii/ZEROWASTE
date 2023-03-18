8143ee47f2e8c2aa25a25a1b5aadf4cd53437ec9
Eduardo Perucci <peruccii2917@hotmail.com>
2023-03-17 21:11:25 -0300

    deployzw

--- src/views/routes.tsx
+++ src/views/routes.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import Login from './login/Login'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './home/Home'
 
 import ProtectedRoutes from './ProtectedRoutes'
